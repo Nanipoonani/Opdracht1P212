18ba3ee147fba1bedd784f262d71875d3245bad2
Nanipoonani <82288594+Nanipoonani@users.noreply.github.com>
2021-04-20 16:42:49 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,7 +5,13 @@ package EnumOpdrachtp212O1;
 public class Main {
     public static void main(String[] args) {
 for (int i = 0; i > Day.values().length; i++) {
-    System.out.println(Day.valueOf(i));
+    Day day0 = Day.values()[0];
+    Day day1 = Day.values()[1];
+    Day day2 = Day.values()[2];
+    Day day3 = Day.values()[3];
+
+
+    System.out.println(day1.MONDAY.ordinal());
 }
     }
 }
