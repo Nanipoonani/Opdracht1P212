fa5231b878e4d5f72ba642bcbef96fca841cc50f
Nanipoonani <82288594+Nanipoonani@users.noreply.github.com>
2021-04-20 16:24:23 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 package EnumOpdrachtp212O1;
 
-//DIT IS EEN TEST
+
 
 public class Main {
     public static void main(String[] args) {
 for (int i = 0; i > Day.values().length; i++) {
-    System.out.println(Day.valueOf());
+    System.out.println(Day.valueOf(i));
 }
     }
 }
